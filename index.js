7e674c5cb4eaca311110a466686f762da76f48de
jamievullo <jvullo@protonmail.com>
2019-11-29 10:02:49 -0500

    Done.

--- index.js
+++ index.js
@@ -5,14 +5,12 @@ function fetchBooks() {
     .then(json => renderBooks(json))
   }
 
-
-
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+    const h4 = document.createElement('h4')
+    h4.innerHTML = `<h4>${book.name}</h4>`
+    main.appendChild(h4)
   })
 }
 
